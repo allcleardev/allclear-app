3d3392ee595574bc497abc6fedc4a7e9c606ca02
happimaker <gregdev1023@gmail.com>
2020-03-29 08:44:46 +0800

    fix: add header-round component

---
+++ src/components/header-round.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const headerStyle = {
+  background: "linear-gradient(to right, #28baff, #1195ff)",
+  borderRadius: "100%",
+  marginLeft: "-50vw",
+  marginRight: "-50vw",
+  padding: "120px 0",
+  textAlign: "center",
+  paddingLeft: "50vw",
+  marginTop: "-40px",
+  paddingRight: "50vw",
+  color: "#FFF",
+  lineHeight: "22px",
+  fontFamily: "'Source Sans Pro', sans-serif",
+  fontSize: "17px"
+}
+
+export default function HeaderRound(props) {
+  return (
+    <div className="d-flex " style={headerStyle}>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
