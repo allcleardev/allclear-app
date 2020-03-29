35234fe47eac4f4005c2c366a9abea1c64d1badc
Naveed Nadjmabadi <naveed.nadjmabadi@inspire.com>
2020-03-28 20:00:19 -0700

    merge Naveed's map changes

--- src/components/map.js
+++ src/components/map.js
@@ -2,17 +2,7 @@ import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/styles";
-<<<<<<< HEAD
 
-import MapGL from "react-map-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import location_data from '../data/locations'
-
-const MAPBOX_TOKEN =
-  "pk.eyJ1IjoibmF2ZWVkbiIsImEiOiJjazg4aWRkczMwNXQxM21rOWFrbGVvNWtpIn0.-k1i0cSw_C5_0aKPlFFtLA";
-
-let location = location_data;
-=======
 import { Map, Marker, Popup, TileLayer, MapControl, withLeaflet } from "react-leaflet";
 import './map.css'
 import locations from '../hooks/airtable'
@@ -32,7 +22,7 @@ function MapPoint(props) {
     </Marker>
   )
 }
->>>>>>> a5c144f... Adds current location to map
+
 
 export default function MapComponent(props) {
     const useStyles = makeStyles(theme => ({
@@ -52,24 +42,6 @@ export default function MapComponent(props) {
     });
   
     return (
-<<<<<<< HEAD
-      <Container className={styles.frame} hidden={value !== index} id={`tabpanel-${index}`}>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Grid item>
-            <MapGL
-              {...viewport}
-              width="80vw"
-              height="70vh"
-  
-              className={styles.frame}
-              mapStyle="mapbox://styles/mapbox/dark-v9"
-              onViewportChange={setViewport}
-              mapboxApiAccessToken={MAPBOX_TOKEN}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-=======
       <Map center={[viewport.latitude, viewport.longitude]} zoom={viewport.zoom} id={`tabpanel-${index}`}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -80,6 +52,5 @@ export default function MapComponent(props) {
 
         {locations.getAll().map((x, idx) => MapPoint({...x, idx}))}
       </Map>
->>>>>>> a5c144f... Adds current location to map
     );
   }
