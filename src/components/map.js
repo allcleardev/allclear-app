70f27ba23d567ff044bbaa199f4a05f3a813117c
Naveed Nadjmabadi <naveed.nadjmabadi@inspire.com>
2020-03-28 20:00:19 -0700

    merge naveed's map  changes

--- src/components/map.js
+++ src/components/map.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/styles";
+<<<<<<< HEAD
 
 import MapGL from "react-map-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -11,6 +12,27 @@ const MAPBOX_TOKEN =
   "pk.eyJ1IjoibmF2ZWVkbiIsImEiOiJjazg4aWRkczMwNXQxM21rOWFrbGVvNWtpIn0.-k1i0cSw_C5_0aKPlFFtLA";
 
 let location = location_data;
+=======
+import { Map, Marker, Popup, TileLayer, MapControl, withLeaflet } from "react-leaflet";
+import './map.css'
+import locations from '../hooks/airtable'
+import GeoSearch from './map-components/geosearch';
+import LocateControl from './map-components/userlocation';
+
+function MapPoint(props) {
+  return (
+    <Marker position={[props.Latitude, props.Longitude]} key={props.idx}>
+      <Popup>
+        Name: {props.Name} <br />
+        Address: {props.Address} <br />
+        Appointment Needed: {props['Appointment Needed']} <br />
+        Drive Through: {props['Drive Through']} <br />
+        Website: {props['Main Website']} <br />
+      </Popup>
+    </Marker>
+  )
+}
+>>>>>>> a5c144f... Adds current location to map
 
 export default function MapComponent(props) {
     const useStyles = makeStyles(theme => ({
@@ -30,6 +52,7 @@ export default function MapComponent(props) {
     });
   
     return (
+<<<<<<< HEAD
       <Container className={styles.frame} hidden={value !== index} id={`tabpanel-${index}`}>
         <Grid container direction="row" justify="center" alignItems="center">
           <Grid item>
@@ -46,5 +69,17 @@ export default function MapComponent(props) {
           </Grid>
         </Grid>
       </Container>
+=======
+      <Map center={[viewport.latitude, viewport.longitude]} zoom={viewport.zoom} id={`tabpanel-${index}`}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoSearch />
+        <LocateControl/>
+
+        {locations.getAll().map((x, idx) => MapPoint({...x, idx}))}
+      </Map>
+>>>>>>> a5c144f... Adds current location to map
     );
   }
