faf10e01bdceecfbd5233676a2146e31194a3509
happimaker <gregdev1023@gmail.com>
2020-03-30 06:44:54 +0800

    style: style phone verify page

--- src/pages/phone-verify.js
+++ src/pages/phone-verify.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from '../components/header-round';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Box from '@material-ui/core/Container';
-import { Button, Input } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Box from "@material-ui/core/Container";
+import { Button, Input } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+import Header from "../components/header-round";
 
 const ContainerStyle = {
   height: "100vh",
@@ -23,29 +25,28 @@ const bodyStyle = {
   // height: "100vh",
   flexDirection: "column",
   alignContent: "space-between"
-}
+};
 
 const useStyles = makeStyles(theme => ({
   margin: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
+    marginRight: theme.spacing(1)
+  }
 }));
 
 export default function PhoneVerify({}) {
-
   const classes = useStyles();
 
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
     checkedF: true,
-    checkedG: true,
+    checkedG: true
   });
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
@@ -56,9 +57,19 @@ export default function PhoneVerify({}) {
         <p>Enter your phone number to get started.</p>
       </Header>
       <FormGroup row style={bodyStyle}>
-        <Input placeholder="Phone Number" fullWidth="true" style={{margin: "120px 0"}}></Input>
-        <h4 style={{color: "#999999", margin:"50px 0"}}>Please review and agree to the <span style={{color: "#007AFF"}}>Terms & Conditions</span> and <span style={{color: "#007AFF"}}>Privacy Policy</span> before continuing</h4>
-        <FormControlLabel style={{width: "85%"}}
+        <Input
+          placeholder="Phone Number"
+          fullWidth="true"
+          style={{ margin: "120px 0" }}
+        ></Input>
+        <h4 style={{ color: "#999999", margin: "50px 0" }}>
+          Please review and agree to the{" "}
+          <span style={{ color: "#007AFF" }}>Terms & Conditions</span> and{" "}
+          <span style={{ color: "#007AFF" }}>Privacy Policy</span> before
+          continuing
+        </h4>
+        <FormControlLabel
+          style={{ width: "85%" }}
           control={
             <Checkbox
               checked={state.checkedB}
@@ -67,11 +78,27 @@ export default function PhoneVerify({}) {
               color="primary"
             />
           }
-          label="I have reviewed and agree to the Terms & Conditions and Privacy Policy" style={{fontSize: "13px !important", color: "#999999", marginLeft: "0"}}
+          label="I have reviewed and agree to the Terms & Conditions and Privacy Policy"
+          style={{
+            fontSize: "13px !important",
+            color: "#999999",
+            marginLeft: "0"
+          }}
         />
-        <Button variant="contained" color="primary" fullWidth="true" style={{height: "48px", backgroundColor: "#007AFF", margin: "60px 0"}}>
-          Verify Phone Number
-        </Button>
+        <Link to="/complete-profile">
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth="true"
+            style={{
+              height: "48px",
+              backgroundColor: "#007AFF",
+              margin: "60px 0"
+            }}
+          >
+            Verify Phone Number
+          </Button>
+        </Link>
       </FormGroup>
     </Box>
   );
