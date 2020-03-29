c05d1875aede63484a086b059185c474b55ed6e0
happimaker <gregdev1023@gmail.com>
2020-03-30 06:13:19 +0800

    fix: style phone verify page

--- src/pages/phone-verify.js
+++ src/pages/phone-verify.js
@@ -55,25 +55,23 @@ export default function PhoneVerify({}) {
         <h1>Phone Number</h1>
         <p>Enter your phone number to get started.</p>
       </Header>
-      <FormGroup row>
-        <div style={bodyStyle}>
-          <Input placeholder="Phone Number" fullWidth="true" style={{margin: "120px 0"}}></Input>
-          <h4 style={{color: "#999999", margin:"50px 0"}}>Please review and agree to the <span style={{color: "#007AFF"}}>Terms & Conditions</span> and <span style={{color: "#007AFF"}}>Privacy Policy</span> before continuing</h4>
-          <FormControlLabel style={{width: "90%"}}
-            control={
-              <Checkbox
-                checked={state.checkedB}
-                onChange={handleChange}
-                name="checkedB"
-                color="primary"
-              />
-            }
-            label="I have reviewed and agree to the Terms & Conditions and Privacy Policy" style={{fontSize: "13px !important", color: "#999999"}}
-          />
-          <Button variant="contained" color="primary" fullWidth="true" style={{height: "48px", backgroundColor: "#007AFF", margin: "60px 0"}}>
-            Verify Phone Number
-          </Button>
-        </div>
+      <FormGroup row style={bodyStyle}>
+        <Input placeholder="Phone Number" fullWidth="true" style={{margin: "120px 0"}}></Input>
+        <h4 style={{color: "#999999", margin:"50px 0"}}>Please review and agree to the <span style={{color: "#007AFF"}}>Terms & Conditions</span> and <span style={{color: "#007AFF"}}>Privacy Policy</span> before continuing</h4>
+        <FormControlLabel style={{width: "85%"}}
+          control={
+            <Checkbox
+              checked={state.checkedB}
+              onChange={handleChange}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          label="I have reviewed and agree to the Terms & Conditions and Privacy Policy" style={{fontSize: "13px !important", color: "#999999", marginLeft: "0"}}
+        />
+        <Button variant="contained" color="primary" fullWidth="true" style={{height: "48px", backgroundColor: "#007AFF", margin: "60px 0"}}>
+          Verify Phone Number
+        </Button>
       </FormGroup>
     </Box>
   );
