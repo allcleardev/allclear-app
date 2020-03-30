af8b6560baa69f0fd4d6af6376c75c425dff96d5
happimaker <gregdev1023@gmail.com>
2020-03-30 20:12:35 +0800

    style: style code file

--- src/components/cardAlertToggle.js
+++ src/components/cardAlertToggle.js
@@ -1,43 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Switch from '@material-ui/core/Switch';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Switch from "@material-ui/core/Switch";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     background: "linear-gradient(to right, #28baff, #1195ff)",
     display: "flex",
     alignItems: "center",
     justifyContent: "space-between",
-    borderRadius: 15,
+    borderRadius: 15
   },
-  content: {
-
-  }
+  content: {}
 }));
 
-export default function AlertToggleCard({title, description}) {
+export default function AlertToggleCard({ title, description }) {
   const classes = useStyles();
-  const theme = useTheme();
 
   const [state, setState] = React.useState({
     checkedA: true,
-    checkedB: true,
+    checkedB: true
   });
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
   return (
     <Card className={classes.root}>
       <CardContent className={classes.content}>
-        <p className="card-title" style={{color: "#FFF"}}>
+        <p className="card-title" style={{ color: "#FFF" }}>
           {title}
         </p>
-        <p className="card-description" style={{color: "#FFF", padding: "5px 0"}}>
+        <p
+          className="card-description"
+          style={{ color: "#FFF", padding: "5px 0" }}
+        >
           {description}
         </p>
       </CardContent>
@@ -45,13 +45,9 @@ export default function AlertToggleCard({title, description}) {
         <Switch
           onChange={handleChange}
           name="checkedA"
-          inputProps={{ 'aria-label': 'secondary checkbox' }}
+          inputProps={{ "aria-label": "secondary checkbox" }}
         />
       </div>
     </Card>
-    
   );
 }
-
-
-
--- src/components/cardArrow.js
+++ src/components/cardArrow.js
@@ -1,43 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-between"
   },
-  content: {
-
-  }
+  content: {}
 }));
 
-export default function ArrowCard({title, description}) {
+export default function ArrowCard({ title, description }) {
   const classes = useStyles();
-  const theme = useTheme();
 
   return (
     <Card className={classes.root}>
       <CardContent className={classes.content}>
-        <p className="card-title" style={{color: "#000"}}>
+        <p className="card-title" style={{ color: "#000" }}>
           {title}
         </p>
-        <p className="card-description" style={{color: "#929292"}}>
+        <p className="card-description" style={{ color: "#929292" }}>
           {description}
         </p>
       </CardContent>
       <div className="btn-arrow">
-        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1 8.59961H15M15 8.59961L8 1.59961M15 8.59961L8 15.5996" stroke="#007AFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <svg
+          width="16"
+          height="17"
+          viewBox="0 0 16 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M1 8.59961H15M15 8.59961L8 1.59961M15 8.59961L8 15.5996"
+            stroke="#007AFF"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
         </svg>
       </div>
     </Card>
-    
   );
 }
-
-
-
