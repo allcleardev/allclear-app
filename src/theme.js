1855999165a879268ed8eaec57d9570497b8139e
happimaker <gregdev1023@gmail.com>
2020-03-30 20:11:06 +0800

    feat: add third color

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,25 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#007AFF',
+      main: "#007AFF"
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b"
+    },
+    third: {
+      main: "#002C83"
     },
     error: {
-      main: red.A400,
+      main: red.A400
     },
     background: {
-      default: '#fff',
-    },
-  },
+      default: "#fff"
+    }
+  }
 });
 
 export default theme;
