a1397e08e6a5168cc627856876429c1fd19be756
Boris Kozak <boriskozqk@gmail.com>
2020-03-30 17:18:50 -0400

    Add some basic filtering capabilities to the map. show more info too.

--- src/components/map.js
+++ src/components/map.js
@@ -2,12 +2,39 @@ import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/styles";
-
+import { withRouter } from 'react-router';
+import queryString from 'query-string';
 import { Map, Marker, Popup, TileLayer, MapControl, withLeaflet } from "react-leaflet";
 import './map.css'
 import locations from '../hooks/airtable'
 import GeoSearch from './map-components/geosearch';
 import LocateControl from './map-components/userlocation';
+import { Router } from "react-router";
+
+
+// Some hacky stuff to demo filtering.  FIXME
+// Should be able to get / set query params using react router
+let appointmentRequired = "Yes"
+let driveThrough = false
+try {
+ appointmentRequired = window.location['href'].split('appointmentRequired=')[1].split("&")[0];
+}
+catch {
+appointmentRequired = "No"
+
+}
+try {
+driveThrough = window.location['href'].split('driveThrough=')[1];
+}
+catch {
+  driveThrough = false
+}
+
+let locationFilter = {
+ "Drive Through" : driveThrough == "true",
+ "Appointment Needed" : appointmentRequired
+}
+
 
 function MapPoint(props) {
   return (
@@ -15,13 +42,15 @@ function MapPoint(props) {
       <Popup>
         Name: {props.Name} <br />
         Address: {props.Address} <br />
+        Hours: {props['Hours']} <br />
         Appointment Needed: {props['Appointment Needed']} <br />
-        Drive Through: {props['Drive Through']} <br />
-        Website: {props['Main Website']} <br />
+        Drive Through: {props['Drive Through'].toString()} <br />
+        <a href={props['Main Website']} target="_blank">Website</a> <br />
       </Popup>
     </Marker>
   )
 }
+
 
 
 export default function MapComponent(props) {
@@ -40,6 +69,7 @@ export default function MapComponent(props) {
       bearing: 0,
       pitch: 0
     });
+    
   
     return (
       <Map center={[viewport.latitude, viewport.longitude]} zoom={viewport.zoom} id={`tabpanel-${index}`}>
@@ -50,7 +80,19 @@ export default function MapComponent(props) {
         <GeoSearch />
         <LocateControl/>
 
-        {locations.getAll().map((x, idx) => MapPoint({...x, idx}))}
+        {locations.getAll().filter(
+          function(f) {
+            //return true;
+            for (var key in locationFilter) {
+              if (f[key] === undefined || f[key] != locationFilter[key])
+                return false;
+            }
+            console.log(f[key])
+
+            return true;
+            //return f['Appointment Needed'] == appointmentRequired
+          }
+        ).map((x, idx) => MapPoint({...x, idx}))}
       </Map>
     );
   }
