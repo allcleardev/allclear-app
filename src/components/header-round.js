35745e58bc2fa8f29b8788c3f3e2ea376a301dd7
happimaker <gregdev1023@gmail.com>
2020-03-30 20:14:54 +0800

    fix: move styles to sass and fix warns

--- src/components/header-round.js
+++ src/components/header-round.js
@@ -1,52 +1,38 @@
 import React from "react";
 
-import { IconButton } from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
 
-const headerStyle = {
-  display: "flex",
-  flexDirection: "column",
-  background: "linear-gradient(to right, #36d1bc, #1195ff)",
-  borderRadius: "100%",
-  marginLeft: "-50vw",
-  marginRight: "-50vw",
-  padding: "120px 0",
-  paddingBottom: "50px",
-  textAlign: "center",
-  paddingLeft: "50vw",
-  marginTop: "-40px",
-  paddingRight: "50vw",
-  color: "#FFF",
-  lineHeight: "22px",
-  fontFamily: "'Source Sans Pro', sans-serif",
-  fontSize: "17px"
-};
+import Logo from "../assets/images/logo.svg";
 
 export default function Header(props) {
   return (
-    <div className="header" style={headerStyle}>
-      <IconButton
-        style={{
-          marginTop: "-25px",
-          justifyContent: "flex-start",
-          display: "block"
-        }}
-      >
-        <svg
-          width="9"
-          height="15"
-          viewBox="0 0 9 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7.59375 13.1428L1.59375 7.14282L7.59375 1.14282"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </IconButton>
+    <div className="header">
+      <div className="header-mobile">
+        <Fab size="small" aria-label="add" className="btn-back-fab">
+          <svg
+            width="9"
+            height="15"
+            viewBox="0 0 9 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.59375 13.1428L1.59375 7.14282L7.59375 1.14282"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Fab>
+      </div>
+      <div className="header-logo">
+        <img src={Logo} alt="Logo" className="logo" />
+        <div className="header-menu">
+          <p>About Us</p>
+          <p>Help</p>
+        </div>
+      </div>
       {props.children}
     </div>
   );
