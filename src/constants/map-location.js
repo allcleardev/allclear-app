ae03acfcc044f1c3afdb9cf9f48b1dba76087802
Boris Kozak <boriskozqk@gmail.com>
2020-04-01 18:47:06 -0400

    use airtable data

--- src/constants/map-location.js
+++ src/constants/map-location.js
@@ -1,3 +1,26 @@
+
+import locations from '../hooks/airtable'
+console.log(locations.locations[0]);
+
+export const mapLocationData = locations.locations.slice(1,3);
+
+/*
+{
+Name: "Urgent Care for Children - Trussville",
+Address: "117 North Chalkville Road Trussville, AL 35173",
+Hours: "2pm - 10 pm Monday - Friday; 10am - 8pm Saturday - Sunday",
+Latitude: 33.6214073,
+Longitude: -86.6086818,
+State: "Alabama",
+Phone: "(205) 848-2273",
+Main Website: "https://www.childrensurgent.com/coronavirus-update-and-testing-hotline/",
+Appointment Needed: "Yes",
+Phone Extension: "",
+Drive Through: "",
+}
+*/
+
+/*
 export const mapLocationData = [
   {
     title: "Lorem Ipsum",
@@ -40,3 +63,4 @@ export const mapLocationData = [
     commute: "Walk-Up"
   }
 ];
+*/
