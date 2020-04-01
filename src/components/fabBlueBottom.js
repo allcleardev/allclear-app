7472fc4ec1178812e10ec943d4c9458801c30faa
happimaker <gregdev1023@gmail.com>
2020-04-02 04:15:27 +0800

    fix: correct props for fab setting

--- src/components/fabBlueBottom.js
+++ src/components/fabBlueBottom.js
@@ -4,26 +4,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    boxShadow: "0px 3px 25px rgba(0, 0, 0, 0.3)",
-    fontSize: "14px",
-    color: "#fff"
-  },
+  root: {},
 
   content: {}
 }));
 
-export default function fabBlueBottom({ children, style }) {
+export default function fabBlueBottom({ children, style, class_name }) {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const classes = useStyles();
 
   return (
-    <Fab
-      aria-label="add"
-      className={classes.root}
-      color="primary"
-      style={style}
-    >
+    <Fab aria-label="add" className={class_name} color="primary" style={style}>
       {children}
     </Fab>
   );
