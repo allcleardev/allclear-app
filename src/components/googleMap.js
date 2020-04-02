67d39b973d20daadcd48d182e2fbe054a98e23e3
happimaker <gregdev1023@gmail.com>
2020-04-03 06:38:26 +0800

    fix: fix google map issue

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import MapMarker from './map-components/mapMarker.jsx';
-
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import MapMarker from "./map-components/mapMarker.jsx";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const locations = ['one','two'];
+const locations = ["one", "two"];
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -18,16 +17,20 @@ class SimpleMap extends Component {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: "100%", width: "100%" }}>
         {console.log(this.props.data.Latitude)}
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAPB7ER1lGxDSZICjq9lmqgxvnlSJCIuYw' }}
+          bootstrapURLKeys={{ key: "AIzaSyAPB7ER1lGxDSZICjq9lmqgxvnlSJCIuYw" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-        {this.props.data.map((result, index) => (
-          <MapMarker lat={result.Latitude} lng={result.Longitude} text={index+1}/>
-        ))}
+          {this.props.data.map((result, index) => (
+            <MapMarker
+              lat={result.Latitude}
+              lng={result.Longitude}
+              text={index + 1}
+            />
+          ))}
         </GoogleMapReact>
       </div>
     );
