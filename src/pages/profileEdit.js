09bab245ed1c899a4d78014165a9d383a02a92fe
happimaker <gregdev1023@gmail.com>
2020-04-04 05:04:33 +0800

    feat: add profile edit page

---
+++ src/pages/profileEdit.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import Header from "../components/homescreen-header";
+import UserProfileCard from "../components/cardProfile";
+import NavBottom from "../components/navBottom";
+import UpdateCriteriaModal from "./updateTestingCriteriaModal";
+
+import Box from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import userAvatar from "../assets/images/avatar.svg";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    width: "100%",
+    margin: "15px 0",
+    borderRadius: "10px",
+    height: 48
+  }
+}));
+
+export default function ProfileEdit() {
+  const classes = useStyles();
+
+  return (
+    <Box className="profile-edit">
+      <Header>
+        <p>Allclear</p>
+      </Header>
+
+      <UserProfileCard
+        avatar={userAvatar}
+        userName="sdf"
+        location="NYC"
+        health="Sympathic"
+        status="untested"
+      ></UserProfileCard>
+      <Grid
+        container
+        spacing={3}
+        className={classes.root}
+        style={{ justifyContent: "center", marginBottom: "56px" }}
+      >
+        <Grid item xs={12} sm={11}>
+          <div className="profile-body flex-direction-col">
+            <h2 className="body-title">Test Location Preferences</h2>
+            <UpdateCriteriaModal></UpdateCriteriaModal>
+          </div>
+        </Grid>
+      </Grid>
+      <NavBottom active={0}></NavBottom>
+    </Box>
+  );
+}
