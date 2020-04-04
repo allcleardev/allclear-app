c3f225aa5e27fb654d0c1d976ec0f188091bf753
happimaker <gregdev1023@gmail.com>
2020-04-05 06:03:02 +0800

    feat: add login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,102 +1,145 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
+import Box from "@material-ui/core/Container";
+import { Button, Grid } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Axios from "axios";
 
+import Header from "../components/header-round";
+import ProgressBottom from "../components/progressBottom";
+import LinearProgress from "@material-ui/core/LinearProgress";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+export default function PhoneVerify({ props }) {
+  const [state, setState] = React.useState({
+    checkedB: true,
+    loading: false,
+  });
 
-export default function SignIn() {
-  const classes = useStyles();
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const verifyPhoneNumber = async () => {
+    setState({ loading: true });
+    const phone = sessionStorage.getItem("phone");
+
+    console.log("phone", phone);
+
+    if (!phone) {
+      //show error message
+      setState({ loading: false });
+      return;
+    }
+    const response = await Axios.post(
+      "https://api-dev.allclear.app/peoples/start",
+      {
+        phone: phone,
+        beenTested: false,
+        haveSymptoms: false,
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        history.push("/phone-verify-success");
+      })
+      .catch((error) => {
+        //show error message
+        setState({ loading: false });
+      });
+  };
+
+  const [value, setValue] = React.useState("");
+
+  const handleCodeChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
+    <div className="background-responsive">
+      <Box className="login">
+        <Header>
+          <h1 style={{ justifyContent: "center", margin: "0" }}>Sign In</h1>
+          <p>Enter your phone number to be sent a verification code.</p>
+        </Header>
+
+        {state.loading === false ? (
+          <form
+            noValidate
+            autoComplete="off"
+            className="body-phone-verify"
+            style={{ textAlign: "center" }}
           >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link to="/forgot" variant="body2">
-                Forgot password?
+            <div className="wid100" style={{ margin: "70px 0" }}>
+              <Grid container justify="center">
+                <Grid item xs={12} sm={6}>
+                  <FormControl
+                    className="form-control"
+                    style={{ height: "100%" }}
+                  >
+                    <TextField
+                      id="outlined-margin-none"
+                      defaultValue=""
+                      className="white-back-input"
+                      variant="outlined"
+                      label={value === "" ? "Phone Number" : ""}
+                      height="60px"
+                      onChange={handleCodeChange}
+                      InputLabelProps={{ shrink: false }}
+                      value={value}
+                      style={{}}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </div>
+            <div className="flexrow wid100 btn-group">
+              <Link to="/create-account" className="wid100-sm">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth="true"
+                  className="button btn-outlined-white btn-full-width font-weight-600 mobile-grey hide-mobile-sm"
+                >
+                  Create Account
+                </Button>
               </Link>
-            </Grid>
-            <Grid item>
-              <Link to="/register">
-                Don't have an account? Sign Up
-              </Link>
+              <Button
+                // onClick={() => sendVerificationCode()}
+                variant="contained"
+                color="primary"
+                fullWidth="true"
+                className="button btn-responsive btn-full-width font-weight-600"
+              >
+                Send Verification Code
+              </Button>
+              <p
+                className="color-primary show-mobile-sm"
+                style={{ padding: "15px 0", display: "none" }}
+              >
+                <strong>Create Account</strong>
+              </p>
+            </div>
+          </form>
+        ) : (
+          <Grid container justify="center">
+            <Grid item xs={12} sm={6}>
+              <LinearProgress color="primary" value="50" />
             </Grid>
           </Grid>
-        </form>
-      </div>
-    </Container>
+        )}
+
+        {state.loading === false ? (
+          <div style={{ padding: "3px 0" }} className="hide-mobile">
+            <ProgressBottom progress="100px"></ProgressBottom>
+          </div>
+        ) : null}
+      </Box>
+    </div>
   );
 }
