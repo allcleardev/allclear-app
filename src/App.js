1630600890f921db30fdb29d552ce3c5ec1a6919
carrie <cstreed@jibe.com>
2020-04-06 13:31:07 -0400

    organized routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import PhoneVerifying from './pages/phoneVerifying';
 import PhoneVerifySuccess from './pages/phone-verify-success';
 import VerifyMagicLink from './pages/verify-magic-link';
 import login from './pages/login';
-import LoginVerification from './pages/loginVerification';
 import loginOrg from './pages/loginOrg';
 import Location from './pages/location';
 import Health from './pages/health';
@@ -35,37 +34,37 @@ export default function App() {
   return (
     <Router>
       <Route exact path="/" component={Launch} />
-      {/* <Route exact path="/" component={Home} /> */}
-      {/* David start */}
-      <Route path="/create-account" component={CreateAccount} />
+      {/* Onboarding Pages */}
       <Route path="/phone-verify" component={PhoneVerify} />
+      <Route path="/phone-verification" component={PhoneVerifyCode} />
+      <Route path="/phone-verifying" component={PhoneVerifying} /> {/* Note: This is a loading screen */}
+      <Route path="/background" component={Background} />
+      <Route path="/conditions" component={Conditions} />
+      <Route path="/symptoms" component={Symptoms} />
+      <Route path="/results" component={Result} /> {/* TODO: Finish style refactor (carrie) */}
+      {/* Login Pages */}
+      <Route path="/login" component={login} />
+      <Route path="/login-verification" component={AuthVerifyCode} />
+      {/* Profile Pages / Settings */}
       <Route path="/complete-profile" component={CompleteProfile} />
-      <Route path="/test" component={Test} />
-      <Route path="/find-test-map" component={FindTestMap} />
-      <Route path="/update-criteria" component={UpdateTestingCenterCriteria} />
       <Route path="/profile-view" component={ProfileView} />
       <Route path="/profile-edit" component={ProfileEdit} />
-      <Route path="/phone-verification" component={PhoneVerifyCode} />
-      <Route path="/phone-verifying" component={PhoneVerifying} />
-      <Route path="/login" component={login} />
-      <Route path="/auth-verification" component={AuthVerifyCode} />
-      <Route path="/login-verification" component={LoginVerification} />
-      {/* David end */}
-      <Route path="/forgot" component={ForgotPassword} />
-      <Route path="/health" component={Health} />
-      <Route path="/add-test" component={AddTest} />
-      <Route path="/location" component={Location} />
-      <Route path="/login-org" component={loginOrg} />
-      <Route path="/symptoms" component={Symptoms} />
-      <Route path="/results" component={Result} />
-      <Route path="/succcess" component={Success} />
-      {/* <Route path="/launch" component={Launch} /> */}
-      <Route path="/conditions" component={Conditions} />
-      <Route path="/background" component={Background} />
-      <Route path="/phone-verify-success" component={PhoneVerifySuccess} />
-      <Route path="/register" component={VerifyMagicLink} />
-      <Route path="/find-test" component={FindTest} />
       <Route path="/result-detail" component={ResultDetail} />
+      <Route path="/add-test" component={AddTest} />
+      <Route path="/health" component={Health} />
+      <Route path="/update-criteria" component={UpdateTestingCenterCriteria} />
+      {/* Map */}
+      <Route path="/find-test-map" component={FindTestMap} />
+      {/* Abandoned??? */}
+      <Route path="/create-account" component={CreateAccount} />
+      <Route path="/phone-verify-success" component={PhoneVerifySuccess} />
+      <Route path="/succcess" component={Success} />
+      <Route path="/register" component={VerifyMagicLink} />
+      <Route path="/forgot" component={ForgotPassword} />
+      <Route path="/login-org" component={loginOrg} />
+      <Route path="/location" component={Location} />
+      <Route path="/test" component={Test} />
+      <Route path="/find-test" component={FindTest} />
     </Router>
   );
 }
