6c72713145cf5ed61d5efc12bee808bb371182fb
Boris Kozak <boriskozak@gmail.com>
2020-04-06 23:18:43 -0400

    fix payload for searching

--- src/services/google-location-svc.js
+++ src/services/google-location-svc.js
@@ -1,15 +1,19 @@
 import Axios from 'axios';
-
+/*eslint-disable */
 export const GetNewPosition = (lat, lng, miles) => {
   return Axios.post(
     'https://api-dev.allclear.app/facilities/search',
     {
-      latitude: lat,
-      longitude: lng,
-      miles,
+        from:
+        {
+            latitude: lat,
+            longitude: lng,
+            miles: miles,
+        }
     },
     {
       headers: { 'X-AllClear-SessionID': sessionStorage.getItem('sessid') },
     },
   );
 };
+/*eslint-enable */
