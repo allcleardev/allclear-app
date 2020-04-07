dc114a19e4ebfbfd6aa35870140c03b8c268d1c7
carrie <cstreed@jibe.com>
2020-04-07 00:36:44 -0400

    feat: ALLCLEAR-162 reorganizing

--- src/App.js
+++ src/App.js
@@ -1,53 +1,52 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Launch from './pages/launch';
-import CreateAccount from './pages/create-account';
+import './styles/app.scss';
+
+import AddTest from './pages/Test/Add-test';
 import AuthVerifyCode from './pages/authVerification';
+import Background from './pages/Background/Background';
+import CompleteProfile from './pages/completeProfile';
+import Conditions from './pages/Conditions/Conditions';
+import CreateAccount from './pages/create-account';
+import FindTest from './pages/ResultDetail/FindTest';
+import FindTestMap from './pages/findTestMap';
+import ForgotPassword from './pages/forgot-password';
+import Health from './pages/health';
+import HealthWorkerStatus from './pages/HealthWorkerStatus/HealthWorkerStatus';
+import Launch from './pages/launch';
+import Location from './pages/location';
+import login from './pages/login';
+import loginOrg from './pages/loginOrg';
 import PhoneVerify from './pages/phoneVerify';
 import PhoneVerifyCode from './pages/phoneVerification';
 import PhoneVerifying from './pages/phoneVerifying';
 import PhoneVerifySuccess from './pages/phone-verify-success';
-import VerifyMagicLink from './pages/verify-magic-link';
-import login from './pages/login';
-import loginOrg from './pages/loginOrg';
-import Location from './pages/location';
-import Health from './pages/health';
+import ProfileEdit from './pages/profileEdit';
+import ProfileView from './pages/profileView';
+import Result from './pages/Result/Result';
+import ResultDetail from './pages/ResultDetail/ResultDetail';
+import Success from './pages/Success/Success';
+import Symptoms from './pages/Symptoms/Symptoms';
 import Test from './pages/test';
 import UpdateTestingCenterCriteria from './pages/updateTestingCenterCriteria';
-import FindTestMap from './pages/findTestMap';
-import ProfileView from './pages/profileView';
-import ProfileEdit from './pages/profileEdit';
-import ForgotPassword from './pages/forgot-password';
-import Success from './pages/Success/Success';
-import Result from './pages/Result/Result';
-import Conditions from './pages/Conditions/Conditions';
-import Symptoms from './pages/Symptoms/Symptoms';
-import CompleteProfile from './pages/completeProfile';
-import Background from './pages/Background/Background';
-import FindTest from './pages/ResultDetail/FindTest';
-import ResultDetail from './pages/ResultDetail/ResultDetail';
-import AddTest from './pages/Test/Add-test';
-import './styles/app.scss';
+import VerifyMagicLink from './pages/verify-magic-link';
 
 export default function App() {
   return (
     <Router>
       <Route exact path="/" component={Launch} />
       {/* Onboarding Pages */}
-      <Route path="/phone-verify" component={PhoneVerify} />
-      <Route path="/phone-verification" component={PhoneVerifyCode} />
-      <Route path="/phone-verifying" component={PhoneVerifying} /> {/* Note: This is a loading screen */}
-      <Route path="/auth-verification" component={AuthVerifyCode} /> {/* Note: This is a loading screen */}
+      <Route path="/sign-up" component={PhoneVerify} />
+      <Route path="/sign-up-verification" component={PhoneVerifyCode} />
+      <Route path="/verifying" component={PhoneVerifying} /> {/* Note: This is a loading screen */}
       <Route path="/background" component={Background} />
-      <Route path="/conditions" component={Conditions} />
+      <Route path="/health-worker" component={HealthWorkerStatus} />
       <Route path="/symptoms" component={Symptoms} />
-      <Route path="/results" component={Result} /> {/* TODO: Finish style refactor (carrie) */}
       {/* Login Pages */}
       <Route path="/login" component={login} />
       <Route path="/login-verification" component={AuthVerifyCode} />
       {/* Profile Pages / Settings */}
-      <Route path="/complete-profile" component={CompleteProfile} />
       <Route path="/profile-view" component={ProfileView} />
       <Route path="/profile-edit" component={ProfileEdit} />
       <Route path="/result-detail" component={ResultDetail} />
@@ -59,6 +58,7 @@ export default function App() {
       {/* Abandoned??? */}
       <Route path="/create-account" component={CreateAccount} />
       <Route path="/phone-verify-success" component={PhoneVerifySuccess} />
+      <Route path="/complete-profile" component={CompleteProfile} />
       <Route path="/succcess" component={Success} />
       <Route path="/register" component={VerifyMagicLink} />
       <Route path="/forgot" component={ForgotPassword} />
@@ -66,6 +66,9 @@ export default function App() {
       <Route path="/location" component={Location} />
       <Route path="/test" component={Test} />
       <Route path="/find-test" component={FindTest} />
+      {/* Definitely Abandoned (for now) */}
+      <Route path="/conditions" component={Conditions} />
+      <Route path="/results" component={Result} />
     </Router>
   );
 }
