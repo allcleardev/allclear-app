4bf78de77156791eaefa259cf05fe8c90da7f573
Joe Sangiorgio <jsangio1@gmail.com>
2020-04-06 22:35:32 -0400

    fix map markers

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,7 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import MapMarker from './map-components/mapMarker.jsx';
-import { GetNewPosition } from '../services/google-location-svc.js';
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -12,26 +11,20 @@ class SimpleMap extends Component {
     zoom: 14,
   };
 
-  onMarkerDragEnd = (evt) => {
-    console.log(evt.center.lat());
-    console.log(evt.center.lng());
-    GetNewPosition(evt.center.lat(), evt.center.lng(), 100).then((result) => console.log(result));
-  };
-
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100%', width: '100%' }}>
+      <div
+        style={{ height: '100%', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAPB7ER1lGxDSZICjq9lmqgxvnlSJCIuYw' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-          onDragEnd={this.onMarkerDragEnd}
         >
           {this.props.data.map((result, index) => (
-            <Fragment key={index}>
-              <MapMarker lat={result.Latitude} lng={result.Longitude} text={index + 1} />
-            </Fragment>
+            <MapMarker
+              key={index}
+              lat={result.Latitude} lng={result.Longitude} text={index + 1} />
           ))}
         </GoogleMapReact>
       </div>
