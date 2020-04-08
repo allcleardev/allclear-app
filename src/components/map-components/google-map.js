1245cc6f72222157924cf6831649d9cb3100e0b1
happimaker <gregdev1023@gmail.com>
2020-04-09 02:07:11 +0800

    fix: get the center of G map session

--- src/components/map-components/google-map.js
+++ src/components/map-components/google-map.js
@@ -16,8 +16,8 @@ class GoogleMap extends Component {
 
   static defaultProps = {
     center: {
-      lat: 40.743992,
-      lng: -74.032364,
+      lat: sessionStorage.getItem('lat'),
+      lng: sessionStorage.getItem('lng'),
     },
     zoom: 12,
   };
