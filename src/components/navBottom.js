3980b294f94a2dcac5c6fa3d981e5ae9de4b6da3
Boris Kozak <boriskozak@gmail.com>
2020-04-09 00:52:31 -0400

    find tests nav button should send user to map

--- src/components/navBottom.js
+++ src/components/navBottom.js
@@ -43,7 +43,7 @@ export default function NavBottom({ active }) {
       <BottomNavigationAction
         label="Find Tests"
         icon={
-          <Link to="/location">
+          <Link to="/map">
           <LocationOnIcon />
           </Link>
         }
