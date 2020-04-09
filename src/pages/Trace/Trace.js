69934629f595d4fd6c95283055331f729c9e01c5
Ravi Chauhan <er.shashankagarwal111.com>
2020-04-10 02:47:18 +0530

    changes in text

--- src/pages/Trace/Trace.js
+++ src/pages/Trace/Trace.js
@@ -25,7 +25,7 @@ class Trace extends React.Component {
                 </div>
 
                 <div className="flex-container flex-just-center flex-direction-col" style={{ height: '75vh', width: '100vw' }}>
-                    <div className="fontsize-34 fontweight-600 color-white aligncenter">Trace With Us</div>
+                    <div className="fontsize-34 fontweight-600 color-white aligncenter">Trace with us,</div>
                     <p className="fontsize-34 fontweight-600 color-white aligncenter">Beat the virus.</p>
                     <p className="fontweight-600 color-white aligncenter" style={{ fontSize: 17, lineHeight: '20px' }}>Comming Soon</p>
                     <p className="color-white aligncenter" style={{ fontSize: '15px', padding: '0px 10px 0px 10px' }}>
