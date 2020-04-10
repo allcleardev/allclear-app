6699dcea4600786c106a8fc9ba46fd4585c5c745
carrie <cstreed@jibe.com>
2020-04-10 15:08:49 -0400

    number is map marker is now centered

--- src/components/map-components/map_marker_styles.js
+++ src/components/map-components/map_marker_styles.js
@@ -5,6 +5,9 @@ const mapMarkerStyle = {
   // initially any map object has left top corner at lat lng coordinates
   // it's on you to set object origin to 0,0 coordinates
   position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
   width: K_WIDTH,
   height: K_HEIGHT,
   left: -K_WIDTH / 2,
