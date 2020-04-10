20d7dc02f38238daf84532895895a598e5ba1c64
carrie <cstreed@jibe.com>
2020-04-10 15:11:47 -0400

    updated marker color to match mock

--- src/components/map-components/map_marker_styles.js
+++ src/components/map-components/map_marker_styles.js
@@ -12,7 +12,7 @@ const mapMarkerStyle = {
   height: K_HEIGHT,
   left: -K_WIDTH / 2,
   top: -K_HEIGHT / 2,
-  backgroundColor: '#3f51b5',
+  backgroundColor: '#002C83',
   borderRadius: K_HEIGHT,
   textAlign: 'center',
   color: 'white',
