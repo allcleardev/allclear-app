49c0035c952e1379c48a321db14e0b0f51d76fed
grangerl330 <grangerl330@gmail.com>
2020-04-11 15:14:49 -0400

    Work in progess

--- src/pages/health.js
+++ src/pages/health.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
---
+++ src/pages/not-found.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { spacing } from '@material-ui/system';
+import { Link } from 'react-router-dom';
+import finn from '../assets/images/finn.png'
+
+
+export default function NotFound() {
+  return (
+    <Grid container spacing={24} alignContent="center" className="bg-primary white flex-container flex-just-center">
+      <Grid item xs={12} className="aligncenter" style={{marginBottom: '10vh'}}>
+        <div className="fontsize-50">404 - page not found</div>
+      </Grid>
+      <Grid container justify="center" spacing={0}>
+        <Grid item xs={12} sm={6} className="aligncenter">
+          <img src={finn} alt="finn-the-dog" style={{height: '50vh', width: '50vh'}} />
+        </Grid>
+        <Grid item xs={12} sm={6} justify='center' className="fontsize-30">
+          <p>We think Finneas ate it.</p>
+          <p>but don't worry, he's a good boy.</p>
+          <div className="wrapBtnS" style={{width: '50%'}}>
+            <Link to="/profile-view">
+              <button className="whiteBGS">Go Home</button>
+            </Link>
+          </div>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
