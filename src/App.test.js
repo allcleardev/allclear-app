14b9bf8c8ecae806f73640954eee45943aeb3f4c
Boris Kozak <boriskozak@gmail.com>
2020-04-12 12:54:41 -0400

    Add basic test harness using jest with puppeteer

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+describe('App loads', () => {
+  test('App splash page loads correctly', async () => {
+    let browser = await puppeteer.launch({
+      headless: true
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.heading');
+
+    browser.close();
+  }, 30000);
+});
