0628056da09529668a88fffab56925c0dec00052
Joe Sangiorgio <jsangio1@gmail.com>
2020-04-12 16:46:43 -0400

    text change and merge from dev

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -8,15 +8,15 @@ export default function NotFound() {
   return (
     <Grid container spacing={24} alignContent="center" className="bg-primary white flex-container flex-just-center">
       <Grid item xs={12} className="aligncenter" style={{marginBottom: '10vh'}}>
-        <div className="fontsize-50">404 - page not found</div>
+        <div className="fontsize-50">404 - Page Not Found</div>
       </Grid>
       <Grid container justify="center" spacing={1}>
         <Grid item xs={12} sm={6} className="aligncenter">
           <img src={finn} alt="finn-the-dog" style={{height: '50vh', width: '50vh'}} />
         </Grid>
         <Grid item xs={12} sm={6} justify='center' className="fontsize-30" style={{marginTop: '5vh', marginLeft: '-8vw'}}>
-          <p>We think Finneas ate it.</p>
-          <p>but don't worry, he's a good boy.</p>
+          <p>We think Finneas ate it!</p>
+          <p>But don't worry; he's a good boy.</p>
           <div className="wrapBtnS" style={{width: '30%'}}>
             <Link to="/profile-view">
               <button className="whiteBGS">Go Home</button>
