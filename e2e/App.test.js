20249dc87adf142447dbd79a4599e322d73008c9
Joe Sangiorgio <jsangio1@gmail.com>
2020-04-12 16:36:27 -0400

    add inspector capabilities to e2e

--- e2e/App.test.js
+++ e2e/App.test.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 
 describe('App loads', () => {
   test('App splash page loads correctly', async () => {
+
     let browser = await puppeteer.launch({
       headless: true
     });
