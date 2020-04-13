315853acf103250a44f1ed2cc9b10dfb32628e0b
Joe Sangiorgio <jsangio1@gmail.com>
2020-04-12 21:41:15 -0400

    make drag handle bigger

--- src/pages/map.page.js
+++ src/pages/map.page.js
@@ -141,11 +141,11 @@ export default function MapPage() {
                 className="side-drawer hide-scrollbar wid100-sm"
               >
                 <Hammer onSwipe={SwipeHandler} options={touchOptions} direction="DIRECTION_VERTICAL">
-                  <div style={{ height: '30px' }} className="geolist-resizer">
+                  <div style={{ height: '60px' }} className="geolist-resizer">
                     <svg width="37" height="6" viewBox="0 0 37 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
-                        d="M2.75977 5.18164C1.37905 5.18164 0.259766 4.06235 0.259766 2.68164C0.259766 
-                      1.30093 1.37905 0.181641 2.75977 0.181641H33.7598C35.1405 0.181641 36.2598 1.30093 
+                        d="M2.75977 5.18164C1.37905 5.18164 0.259766 4.06235 0.259766 2.68164C0.259766
+                      1.30093 1.37905 0.181641 2.75977 0.181641H33.7598C35.1405 0.181641 36.2598 1.30093
                       36.2598 2.68164C36.2598 4.06235 35.1405 5.18164 33.7598 5.18164H2.75977Z"
                         fill="#aaadb3"
                       />
@@ -160,7 +160,7 @@ export default function MapPage() {
                     color="primary"
                     fullWidth
                     startIcon={SettingsSVG()}
-                    style={{ margin: '20px 0' }}
+                    style={{ margin: '0px 0px 20px 0px' }}
                     onClick={() => {
                       modalService.toggleModal('criteria', true);
                     }}
