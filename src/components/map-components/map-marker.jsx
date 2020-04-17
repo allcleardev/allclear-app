b40ac021c92cbf0ea3a252055852e75d733a3cd4
Brian Parrish <bparrish@jibe.com>
2020-04-16 23:01:02 -0400

    AC-235: last element now scrolls into view after expansion panel has opened so it hits the bottom

--- src/components/map-components/map-marker.jsx
+++ src/components/map-components/map-marker.jsx
@@ -4,16 +4,20 @@ import { mapMarkerStyle } from './map_marker_styles.js';
 // import {AppContext} from '../../contexts/App.context';
 
 export default function MapMarker(props) {
-  const { index } = props;
+  const { index, length } = props;
   // const {appState, setAppState} = useContext(AppContext);
   return (
     <div
       onClick={(e, i) => {
         // todo: dont touch dom lol, do this with refs (much harder)
         const elemToOpen = document.querySelectorAll('.MuiExpansionPanel-root')[index];
-        elemToOpen.scrollIntoView({ behavior: 'smooth' });
         const isCurrentlyExpanded = [].slice.call(elemToOpen.classList).includes('Mui-expanded');
         if (!isCurrentlyExpanded) elemToOpen.children[0].click();
+        const isLastElement = index === length - 1;
+        if (isLastElement) {
+          // wait for expansion panel animation to end before scrolling
+          setTimeout(() => elemToOpen.scrollIntoView({ behavior: 'smooth' }), 300);
+        } else elemToOpen.scrollIntoView({ behavior: 'smooth' });
 
         // todo: instead of touching dom, utilize previously set state and ref markers to accomplish this?
         // setAppState({
