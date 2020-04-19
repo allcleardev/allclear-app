032780e0330704adba4233ad3d09e3cd0422e216
carrie <cstreed@jibe.com>
2020-04-19 04:33:36 -0400

    ALLCLEAR-300 layout in place for homescreen + locations are dynamically loaded

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -1,57 +1,48 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { bindAll } from 'lodash';
+import { bindAll, get } from 'lodash';
 
 import Header from '../components/general/headers/header';
 import BottomNav from '../components/general/navs/bottom-nav';
 import UserAvatar from '../assets/images/defaultProfile.svg';
-import PeopleService from '../services/people.service.js';
-import { AppContext, INITIAL_APP_STATE } from '../contexts/app.context';
 import SettingsIcon from '../assets/images/settings-icon.svg';
+import PersonShareIcon from '../assets/images/person-share-icon.svg';
+import { AppContext } from '../contexts/app.context';
 
 import Container from '@material-ui/core/Container';
-import { Button, IconButton, Chip } from '@material-ui/core';
+import WarningRoundedIcon from '@material-ui/icons/WarningRounded';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'; // TODO: Ask Ashley for uncorrupted svg version from her mock
+import { Button } from '@material-ui/core';
 
 export default class HomePage extends Component {
   static contextType = AppContext;
 
   constructor(props) {
     super(props);
-    bindAll(this, ['componentDidMount', 'fetchProfile', 'setProfile']);
-    this.peopleService = PeopleService.getInstance();
-    this.state = {
-      profile: {},
-    };
+    bindAll(this, ['componentDidMount', 'routeChange', 'onShareClicked']);
   }
 
-  async componentDidMount() {
-    if (!localStorage.getItem('session')) {
-      return this.props.history('/sign-up');
-    }
-    const session = JSON.parse(localStorage.getItem('session'));
-    console.log('SESSION:', session);
+  componentDidMount() {
+    const { appState } = this.context;
 
-    this.setProfile(session);
-    // await this.fetchProfile(session);
+    this.testLocations = get(appState, 'map.locations') || [];
+    this.testLocations.reduce((arr, item, index) => {
+      if (index > 6) {
+        arr.push(item);
+      }
+
+      return arr;
+    }, []);
+    console.log('LOCATIONS:', this.testLocations);
   }
 
-  async fetchProfile(session) {
-    const response = await this.peopleService.getById(session.person.id);
-    const profile = response.data;
-
-    session.person = profile;
-    localStorage.setItem('session', JSON.stringify(session));
-    this.setState({ profile });
+  routeChange(route) {
+    this.props.history.push(route);
   }
 
-  setProfile(session) {
-    if (session.person) {
-      this.setState({ profile: session.person });
-    }
-  }
+  onShareClicked() {}
 
   render() {
-    const profile = this.state.profile;
     return (
       <section className="home-page">
         <Link to="/settings" className="settings-option hide-desktop">
@@ -84,23 +75,53 @@ export default class HomePage extends Component {
             <img src={SettingsIcon} className="settings-option__icon" alt="Settings" />
           </Link>
 
-          <article className="card">
-            <dl className="card__content">
-              <dt className="card__term">Phone</dt>
-              <dd className="card__description"> phone number</dd>
-            </dl>
+          <article className="banner article">
+            <WarningRoundedIcon className="banner__icon"></WarningRoundedIcon>
+            <p className="banner__text">
+              Your profile is not prioritized for testing per CDC Criteria. <a href="/">Learn More</a>
+            </p>
           </article>
 
-          <Button
-            onClick={() => this.executeLogout()}
-            style={{ color: '#2A7DF4', border: '1px solid #2A7DF4' }}
-            className="btn-big  fontsize-16"
-          >
-            Logout
-          </Button>
+          <article className="locations article">
+            <h2 className="sub-heading">
+              Test Locations Near You <InfoOutlinedIcon className="info-icon"></InfoOutlinedIcon>
+            </h2>
+
+            {this.testLocations && this.testLocations.length
+              ? this.testLocations.slice(0, 5).map((location) => (
+                  <article className="card">
+                    <dl className="card__content">
+                      <dt className="card__term">{location.name}</dt>
+                      <dd className="card__description">{location.address}</dd>
+                      <dd className="card__description">Lorem ipsum dolor sit amet, consectetur.</dd>
+                    </dl>
+                  </article>
+                ))
+              : 'No Results Found'}
+
+            <Button
+              fullWidth
+              variant="outlined"
+              onClick={() => this.routeChange('/profile')}
+              style={{ color: '#2A7DF4', border: '1px solid #2A7DF4' }}
+              className=""
+            >
+              View More
+            </Button>
+          </article>
+
+          <article className="share article">
+            <h2 className="sub-heading">
+              Friends <InfoOutlinedIcon className="info-icon"></InfoOutlinedIcon>
+            </h2>
+            <Button fullWidth variant="contained" onClick={() => this.onShareClicked()} className="share__button">
+              <img src={PersonShareIcon} className="share__icon" alt="Share" />
+              <span>Share allclear</span>
+            </Button>
+          </article>
         </Container>
 
-        <BottomNav active={3}></BottomNav>
+        <BottomNav active={0}></BottomNav>
       </section>
     );
   }
