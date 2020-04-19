10cbf8a58d585d4c7ba4890204ab166990e5967e
Joe Sangiorgio <jsangio1@gmail.com>
2020-04-19 14:51:19 -0400

    fix session related profile junk

--- src/pages/profile-edit.page.js
+++ src/pages/profile-edit.page.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { bindAll } from 'lodash';
+import React, {Component} from 'react';
+import {bindAll} from 'lodash';
 
 import HomescreenHeader from '../components/general/headers/header-homescreen';
 import BottomNav from '../components/general/navs/bottom-nav';
@@ -9,8 +9,8 @@ import PeopleService from '../services/people.service.js';
 import TypesService from '../services/types.service.js';
 
 import Container from '@material-ui/core/Container';
-import { Button, FormControl, Select, MenuItem } from '@material-ui/core';
-import { AppContext } from '@contexts/app.context';
+import {Button, FormControl, Select, MenuItem} from '@material-ui/core';
+import {AppContext} from '@contexts/app.context';
 import MultiSelectInput from '@general/inputs/multi-select-input';
 
 export default class ProfileEditPage extends Component {
@@ -47,10 +47,7 @@ export default class ProfileEditPage extends Component {
   }
 
   async componentDidMount() {
-    if (!localStorage.getItem('session')) {
-      return this.props.history('/sign-up');
-    }
-    this.setState({ loading: true });
+    this.setState({loading: true});
     const currState = this.context.appState;
 
     this.setProfile(currState);
@@ -60,7 +57,7 @@ export default class ProfileEditPage extends Component {
 
   async fetchHealthWorkerStatuses() {
     const response = await this.typesService.getHealthWorkerStatuses();
-    this.setState({ healthWorkerStatusList: response });
+    this.setState({healthWorkerStatusList: response});
   }
 
   async fetchSymptoms() {
@@ -73,7 +70,7 @@ export default class ProfileEditPage extends Component {
         selectedSymptoms.every((selectedOption) => selectedOption.id !== responseOption.id),
       ),
     );
-    this.setState({ symptomsList: combined });
+    this.setState({symptomsList: combined});
   }
 
   setProfile(session) {
@@ -82,15 +79,15 @@ export default class ProfileEditPage extends Component {
 
       // need to pull out user selected symptoms and set to own state for isolated data handling
       if (profile.symptoms && profile.symptoms.length) {
-        this.setState({ userSelectedSymptoms: profile.symptoms });
+        this.setState({userSelectedSymptoms: profile.symptoms});
       }
-      this.setState({ profile, loading: false });
+      this.setState({profile, loading: false});
     }
   }
 
   handleLocationSelection(bool, value) {
     if (value) {
-      const { latitude, longitude } = value;
+      const {latitude, longitude} = value;
       this.setState({
         newProfile: {
           ...this.state.newProfile,
@@ -105,27 +102,28 @@ export default class ProfileEditPage extends Component {
   handleHealthWorkerSelection(event) {
     if (event.target.value) {
       this.setState({
-        newProfile: { ...this.state.newProfile, healthWorkerStatusId: event.target.value },
+        newProfile: {...this.state.newProfile, healthWorkerStatusId: event.target.value},
       });
     }
   }
 
   handleSymptomsSelection(symptomList) {
-      this.setState({
-        // set chips/dropdown selection state
-        userSelectedSymptoms: symptomList,
-        // update newProfile to be sent to the server on submit
-        newProfile: { ...this.state.newProfile, symptoms: symptomList },
-      });
+    this.setState({
+      // set chips/dropdown selection state
+      userSelectedSymptoms: symptomList,
+      // update newProfile to be sent to the server on submit
+      newProfile: {...this.state.newProfile, symptoms: symptomList},
+    });
   }
 
   async onUpdateProfileClicked() {
-    const { appState, setAppState } = this.context;
+    const {appState, setAppState} = this.context;
+    const session = this.context.appState.sessionId;
     const updatedProfile = {
       ...this.state.profile,
       ...this.state.newProfile,
     };
-    await this.peopleService.editProfile(updatedProfile);
+    await this.peopleService.editProfile(updatedProfile, session);
     setAppState({
       ...appState,
       person: {
@@ -151,11 +149,11 @@ export default class ProfileEditPage extends Component {
               {this.state.loading ? (
                 ''
               ) : (
-                <GoogleMapsAutocomplete
-                  initialValue={profile.locationName}
-                  locationSelected={this.handleLocationSelection}
-                ></GoogleMapsAutocomplete>
-              )}
+                 <GoogleMapsAutocomplete
+                   initialValue={profile.locationName}
+                   locationSelected={this.handleLocationSelection}
+                 ></GoogleMapsAutocomplete>
+               )}
             </div>
 
             <div className="card__content">
@@ -164,20 +162,20 @@ export default class ProfileEditPage extends Component {
                 {this.state.loading ? (
                   ''
                 ) : (
-                  <Select
-                    className="input"
-                    defaultValue={profile.healthWorkerStatusId}
-                    onChange={this.handleHealthWorkerSelection}
-                  >
-                    {this.state.healthWorkerStatusList
+                   <Select
+                     className="input"
+                     defaultValue={profile.healthWorkerStatusId}
+                     onChange={this.handleHealthWorkerSelection}
+                   >
+                     {this.state.healthWorkerStatusList
                       ? this.state.healthWorkerStatusList.map((option) => (
-                          <MenuItem value={option.id} key={option.id}>
-                            {option.name}
-                          </MenuItem>
-                        ))
+                         <MenuItem value={option.id} key={option.id}>
+                           {option.name}
+                         </MenuItem>
+                       ))
                       : ''}
-                  </Select>
-                )}
+                   </Select>
+                 )}
               </FormControl>
             </div>
 
