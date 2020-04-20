4108ffdc6be689c3f99d698f067a50da9b252b0f
vvendemia@jibe.com <vvendemia@jibe.com>
2020-04-20 08:00:38 -0400

    ALLCLEAR-280 Add tests up to Health Worker Status page

--- e2e/App.test.js
+++ e2e/App.test.js
@@ -1,20 +1,18 @@
+const config = require('dotenv').config();
 const puppeteer = require('puppeteer');
-const accountSid = 'ACc5e6ed7ed6c090937eedf9090f8f21fe';
-const authToken = '';
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(
+  config.parsed.TWILIO_E2E_SID, config.parsed.TWILIO_E2E_SECRET
+);
 const e2ePhoneNumber = '6466030984';
 var verificationCode = "";
 
 
+const takeScreenshot = async (page, name) => {
+  page.screenshot({path: `./e2e/screenshots/${name}.png`});
+}
+
 describe('App loads', () => {
   test('App splash page loads correctly', async () => {
-
-  // Delete al text messages
-  // console.log("deleting old messages")
-  // client.messages.list({limit: 20, to: '+1' + e2ePhoneNumber})
-  //   .then(messages => messages.forEach(m => 
-  //     client.messages(m.sid).remove()
-  //     ));
 
     let browser = await puppeteer.launch({
       headless: true
@@ -29,39 +27,78 @@ describe('App loads', () => {
       userAgent: ''
     });
 
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.heading');
-    await page.focus('[name="phone"]');
-    await page.type('[name="phone"]',e2ePhoneNumber);
-    await page.click('[name="termsAndConditions"]');
-    await page.click('[name="alertable"]');
-    const [button] = await page.$x("//button[contains(., 'Send Verification Code')]");
-    if (button) {
-      await button.click();
-    }
-    await page.waitForSelector('[placeholder="Enter Code"]');
-    client.messages.list({limit: 20, to: '+1' + e2ePhoneNumber})
-      .then(messages => messages.forEach(m => {
-        console.log(m.body);
-        verificationCode = m.body.match(/[0-9]{6}/g)[0]
-        console.log('got')
-        console.log(m.body.match(/[0-9]{6}/g))
-        console.log(verificationCode);
-      }));
-
-    await page.type('[placeholder="Enter Code"]',verificationCode);
-    await page.waitForSelector('.heading');
-    await page.screenshot({path: 'buddy-screenshot.png'});
-
-    await page.focus('[placeholder="Enter Code"]');
-    await page.type('[placeholder="Enter Code"]',verificationCode);
-    await page.click('.next');
-    await delay(2000);
-    await page.focus('#google-maps-autocomplete');
-    await page.type('#google-maps-autocomplete',"Dallas TX");
-
-    await page.screenshot({path: 'screenshot.png'});
+    await page.goto('http://localhost:3000/get-started');
+    await page.waitForXPath('.//button[contains(@class, "signup")]');
 
     browser.close();
+  }, 20000);
+
+  test('Can navigate to next page', async () => {
+    let browser = await puppeteer.launch({
+      headless: true
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:3000/get-started');
+    await page.waitForXPath('.//button[contains(@class, "signup")]');
+    let button = await page.$x('.//button[contains(@class, "signup")]');
+    button[0].click();
+
+    await page.waitForNavigation();
+
+    let header = await page.$x('.//h1[contains(@class, "heading")]');
+    var text = await (await header[0].getProperty('textContent')).jsonValue();
+    expect(text).toBe('Background');
+
+  }, 20000);
+
+  test('Can input location on background page', async () => {
+    let browser = await puppeteer.launch({
+      headless: true
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:3000/background');
+    await page.waitForXPath('.//input[@id="google-maps-autocomplete"]');
+    await page.type('#google-maps-autocomplete', '11211')
+
+    // wait for popup to show
+    await page.waitForSelector('#google-maps-autocomplete-popup');
+
+    await takeScreenshot(page, 'location-loaded')
+
+    // select the first option in the list
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('Enter');
+
+    // wait for the button to be enabled
+    await page.waitFor(4000)
+    let button = await page.$x('.//button[contains(@class, "next")]');
+    button[0].click();
+
+    await page.waitForNavigation();
+
+    await takeScreenshot(page, 'on-health-worker-page')
+
+    let header = await page.$x('.//h1[@class="heading"]');
+    var text = await (await header[0].getProperty('textContent')).jsonValue();
+    expect(text).toBe('Health Worker Status');
+
   }, 30000);
 });
