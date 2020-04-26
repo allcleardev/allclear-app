4983c642db32ad4edfed9a97cc49aab8f9984499
Carrie Streed <15806602+carriestreed@users.noreply.github.com>
2020-04-26 12:27:24 -0400

    ALLCLEAR-303 added severity prop + updated component base styles

--- src/components/general/alerts/snackbar-message.js
+++ src/components/general/alerts/snackbar-message.js
@@ -3,17 +3,27 @@ import Alert from '@material-ui/lab/Alert';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: '60%',
+    fontSize: '.95rem',
+    [theme.breakpoints.up('sm')]: {
+      minWidth: 400,
+    },
+  },
+}));
 
 function SlideTransition(props) {
-  return <Slide {...props} direction="up"/>;
+  return <Slide {...props} direction="up" />;
 }
 
 export default function SnackbarMessage(props) {
+  const classes = useStyles();
 
   return (
-    <Paper
-      elevation={16}
-    >
+    <Paper elevation={16}>
       <Snackbar
         open={props.isOpen}
         TransitionComponent={SlideTransition}
@@ -21,7 +31,7 @@ export default function SnackbarMessage(props) {
         onClose={props.onClose}
         className={props.snackbarClass || 'snackbar__error'}
       >
-        <Alert onClose={props.onClose} severity="error">
+        <Alert className={classes.root} onClose={props.onClose} severity={props.severity || 'error'}>
           {props.message}
         </Alert>
       </Snackbar>
