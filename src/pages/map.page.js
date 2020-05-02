0c21bf01508efad1d9997a2a61f51a19e82d095d
Carrie Streed <15806602+carriestreed@users.noreply.github.com>
2020-05-02 01:03:48 -0400

    fixed unscrollable issue found in onboarding flow

--- src/pages/map.page.js
+++ src/pages/map.page.js
@@ -224,6 +224,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column-reverse',
     width: '100%',
+    height: '100%',
     [theme.breakpoints.up('md')]: {
       flexDirection: 'row',
     },
