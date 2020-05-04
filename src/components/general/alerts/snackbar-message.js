9c3beeb21f869f28b09552bf4bb217e2b802713e
Carrie Streed <15806602+carriestreed@users.noreply.github.com>
2020-05-04 13:27:42 -0400

    removed paper elevation. didnt work + added dropshadow to page randomly

--- src/components/general/alerts/snackbar-message.js
+++ src/components/general/alerts/snackbar-message.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Alert from '@material-ui/lab/Alert';
 import Snackbar from '@material-ui/core/Snackbar';
 import Slide from '@material-ui/core/Slide';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -23,22 +22,16 @@ export default function SnackbarMessage(props) {
   const classes = useStyles();
 
   return (
-    <Paper elevation={16}>
-      <Snackbar
-        open={props.isOpen}
-        TransitionComponent={SlideTransition}
-        autoHideDuration={props.duration || 4000}
-        onClose={props.onClose}
-        className={props.snackbarClass || 'snackbar__error'}
-      >
-        <Alert
-          className={classes.root}
-          onClose={props.onClose}
-          severity={props.severity || 'error'}
-        >
-          {props.message}
-        </Alert>
-      </Snackbar>
-    </Paper>
+    <Snackbar
+      open={props.isOpen}
+      TransitionComponent={SlideTransition}
+      autoHideDuration={props.duration || 4000}
+      onClose={props.onClose}
+      className={props.snackbarClass || 'snackbar__error'}
+    >
+      <Alert className={classes.root} onClose={props.onClose} severity={props.severity || 'error'}>
+        {props.message}
+      </Alert>
+    </Snackbar>
   );
 }
