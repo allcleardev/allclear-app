271b879f1135c96fa9af0ac016fbd420c57ae1a4
Joe Sangiorgio <jsangio1@gmail.com>
2020-05-06 00:38:00 -0400

    last revert before merge

--- stampBuild.js
+++ stampBuild.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
-const {SitemapStream} = require('sitemap');
-const {createWriteStream} = require('fs');
-const {resolve} = require('path');
-const axios = require('axios');
-const {set, keys, forEach, bindAll} = require('lodash');
-const rateLimit = require('axios-rate-limit');
-const { execSync } = require('child_process');
+
+// add these if needed later
+// const { resolve } = require('path');
+// let packageJSON;
+// try {
+//   packageJSON = require(resolve(__dirname, './package.json'));
+// } catch (err) {
+//   console.log('cant find packageJSON', err);
+// }
+
+// const version = packageJSON ? packageJSON.version : 'NA-Version';
+// const name = packageJSON ? packageJSON.name : 'NA-name';
 
 const date = new Date();
 const fullDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 const time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 
 let baseURL;
-let currBranch = process.env.GIT_BRANCH;
 
 if (process.env.GIT_BRANCH === 'master') {
   baseURL = 'https://api.allclear.app';
@@ -30,195 +34,3 @@ let envFileParsed = envfile.parseFileSync('.env');
 envFileParsed.REACT_APP_BUILT_AT = `${fullDate} - ${time}`;
 envFileParsed.REACT_APP_BASE_URL = baseURL;
 fs.writeFileSync('./.env', envfile.stringifySync(envFileParsed));
-
-// -----
-
-
-
-class SiteBuilder {
-
-  constructor() {
-    this.api = rateLimit(axios.create(), {maxRequests: 75, perMilliseconds: 1000});
-    // const currBranch = execSync(`git rev-parse --abbrev-ref HEAD`);
-    // const currBranch = process.env.GIT_BRANCH;
-
-    // todo: use process.env, its not working here.
-    if (currBranch === 'master') {
-      this.baseURL = 'https://api.allclear.app';
-      this.appUrl = 'https://go.allclear.app';
-    } else if (currBranch === 'staging') {
-      this.baseURL = 'https://api-staging.allclear.app';
-      this.appUrl = 'https://app-staging.allclear.app';
-    } else if (currBranch === 'dev') {
-      this.baseURL = 'https://api-dev.allclear.app';
-      this.appUrl = 'https://app-dev.allclear.app';
-    } else {
-      this.baseURL = 'https://api-staging.allclear.app';
-      this.appUrl = 'https://app-staging.allclear.app';
-    }
-
-    console.log('--- Creating Sitemap With These URLs: ---')
-    console.log('baseURL', this.baseURL);
-    console.log('appUrl', this.appUrl);
-
-    this.failures = {};
-
-    bindAll(this, [
-      'getStates',
-      'getCities',
-      'build',
-      'populateCities',
-      'populateAllSitesInCity',
-    ]);
-
-    // if(currBranch === 'staging' || currBranch === 'master'){
-    //   console.log(`Building on branch: ${currBranch}`);
-    //   this.build();
-    // }else{
-    //   console.log(`Skipping build on branch: ${currBranch}`);
-    // }
-
-      console.log(`Branch: ${currBranch}`);
-    this.build();
-
-  }
-
-  async getLocations(body) {
-    return this.api({
-      method: 'POST',
-      headers: {'content-type': 'application/json'},
-      url: `${this.baseURL}/facilities/search`,
-      data: {
-        ...body,
-        'pageSize': 500 // todo: maybe this is not enough in the future
-      },
-    });
-  }
-
-  async getStates() {
-    return this.api({
-      method: 'GET',
-      headers: {'content-type': 'application/json'},
-      url: `${this.baseURL}/facilities/states`
-    });
-  }
-
-  async getCities(state) {
-    return this.api({
-      method: 'GET',
-      url: `${this.baseURL}/facilities/cities`,
-      params: {state}
-    });
-  }
-
-  populateAllSitesInCity() {
-    let allPromises = [];
-
-    // loop thru states
-    forEach(this.stateMap, async (e, state) => {
-
-      // loop thru cities
-      forEach(this.stateMap[state].cities, (e, city) => {
-
-        // call a search for all facilities within a city
-        const currPromise = this.getLocations({city, state})
-          .then(({data}) => {
-            const {records} = data;
-            const dashedCity = dashStr(city);
-            const dashedState = dashStr(state);
-            set(this, `stateMap.${state}.cities.${city}`, {...e, records});
-
-            // finally, write out this facility route to the sitemap
-            forEach(records, (facility) => {
-              const dashedName = dashStr(facility.name);
-              const {id} = facility;
-              // todo: comment this in for both name and id
-              // this.smStream.write({url: `/locations/${dashedState}/${dashedCity}/${dashedName}`});
-              this.smStream.write({url: `/locations/${dashedState}/${dashedCity}/${id}`});
-            });
-
-          })
-          .catch((err) => {
-            this.failures[city] = {...err, location: `${state},${city}` };
-          });
-        allPromises.push(currPromise);
-
-      });
-    });
-
-    Promise.all(allPromises)
-      .then(() => {
-        console.log('-------------------');
-        console.log('Sitemap Completed!');
-        console.log('-------------------');
-        console.log(`${keys(this.failures).length} Failures ⬇`);
-
-        forEach(this.failures, (e) => {
-          console.log(e.location);
-        });
-
-        this.smStream.end();
-        this.smStream.pipe(createWriteStream(resolve('./public/sitemap.xml')));
-      });
-  }
-
-  populateCities() {
-    let allPromises = [];
-    forEach(this.stateMap, async (e, state) => {
-      const currPromise = this.getCities(state).then(({data}) => {
-        forEach(data, (city) => {
-          const cityName = city.name;
-          const dashedCity = dashStr(cityName);
-          const dashedState = dashStr(state);
-          set(this, `stateMap.${state}.cities.${cityName}`, {total: city.total});
-          this.smStream.write({url: `/locations/${dashedState}/${dashedCity}`});
-        });
-      });
-      allPromises.push(currPromise);
-    });
-
-    Promise.all(allPromises).then(() => {
-      console.log('=== Populating Centers Per City . Please Hold... ===')
-      this.populateAllSitesInCity();
-    });
-  }
-
-  async build() {
-    try {
-      this.smStream = new SitemapStream({hostname: this.appUrl});
-
-      this.smStream.write({url: '/map'});
-      this.smStream.write({url: '/contact-tracing'});
-      this.smStream.write({url: '/create-account'});
-      this.smStream.write({url: '/state-list'});
-
-      this.stateMap = {};
-      console.log('Populating States');
-      await this.getStates()
-        .then((resp) => {
-
-          forEach(resp.data, (e2) => {
-            this.stateMap[e2.name] = {};
-          });
-
-          console.log('Populating Cities');
-          this.populateCities();
-
-        })
-        .catch((e, i) => {
-
-        });
-
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-}
-
-function dashStr(str) {
-  return str.replace(/ /g, '-');
-}
-
-const builder = new SiteBuilder();
-
