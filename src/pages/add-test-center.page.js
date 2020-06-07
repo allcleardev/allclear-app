02ba9970b9f2156e320fb04afb9fadf7c4e26f09
carrie <15806602+carriestreed@users.noreply.github.com>
2020-06-07 19:50:35 -0400

    ALLCLEAR-560 UI is set

--- src/pages/add-test-center.page.js
+++ src/pages/add-test-center.page.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import { bindAll } from 'lodash';
 import styled from 'styled-components';
 
+import {
+  Container,
+  TextField,
+  MenuItem,
+  Select,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  FormLabel,
+  FormControl,
+} from '@material-ui/core';
+
+import { AppContext } from '@contexts/app.context';
+import { LOCATION_PROFILE_RADIO_OPTIONS, OFFERINGS, SCREENING_METHODS } from '@constants/add-test-center.constants';
+import Header from '@components/general/headers/header';
+import PrimaryButton from '@components/general/buttons/primary-button';
 import PeopleService from '@services/people.service';
 import FacilityService from '@services/facility.service.js';
-
-import Header from '@components/general/headers/header';
-import { AppContext } from '@contexts/app.context';
-
-import { Container, TextField, Select, MenuItem, FormControl } from '@material-ui/core';
 
 export default class AddTestCenterPage extends Component {
   static contextType = AppContext;
@@ -16,14 +27,24 @@ export default class AddTestCenterPage extends Component {
 
   constructor(props) {
     super(props);
-    bindAll(this, []);
+    bindAll(this, ['handleSubmit']);
     this.peopleService = PeopleService.getInstance();
     this.facilityService = FacilityService.getInstance();
   }
 
+  handleChange(option, index) {
+    console.log('option', option);
+    console.log('CHANGE:', index);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit', e);
+  };
+
   render() {
     return (
-      <section className="add-test-center-page">
+      <Section className="add-test-center-page">
         <StyledHeader>
           <h1>Submit New Test Center</h1>
           <h2>
@@ -33,74 +54,209 @@ export default class AddTestCenterPage extends Component {
         </StyledHeader>
 
         <Container maxWidth="sm">
-          <FormHeading>Test Center Profile</FormHeading>
-          <Form noValidate autoComplete="off">
-            <TextField
-              id="test-center-name"
-              className="input"
-              variant="outlined"
-              placeholder="Test Center Name*"
-              helperText="*Required"
-              required
-            />
+          <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+            <Article>
+              <H3>Test Center Profile</H3>
+              <TextField
+                id="test-center-name"
+                className="input"
+                variant="outlined"
+                placeholder="Test Center Name*"
+                helperText="*Required"
+                required
+              />
 
-            <TextField
-              id="test-center-address"
-              className="input"
-              variant="outlined"
-              placeholder="Test Center Address (or locadtion description if address is unavailable)*"
-              helperText="*Required"
-              rows={4}
-              multiline
-              required
-            />
+              <TextField
+                id="test-center-address"
+                className="input"
+                variant="outlined"
+                placeholder="Test Center Address (or test center description if address is unavailable)*"
+                helperText="*Required"
+                rows={4}
+                multiline
+                required
+              />
 
-            <FormControl variant="outlined" className="input">
-              <Select
-                displayEmpty
-                inputProps={{ 'aria-label': 'Without label' }}
-                // value={10}
-                // onChange={handleChange}
-              >
-                <MenuItem value="" disabled>
-                  Test Center Type
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
+              <FormControl variant="outlined" className="input">
+                <Select
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                  // value={10}
+                  // onChange={handleChange}
+                >
+                  <MenuItem value="" disabled>
+                    Test Center Type
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
 
-            <TextField
-              id="test-center-notes"
-              className="input"
-              variant="outlined"
-              placeholder="Test Center Additional Notes (email, phone number, website, etc.)"
-              rows={4}
-              multiline
-            />
-          </Form>
+              <TextField
+                id="test-center-notes"
+                className="input"
+                variant="outlined"
+                placeholder="Test Center Additional Notes (email, phone number, website, etc.)"
+                rows={4}
+                multiline
+              />
+
+              <FormControl component="fieldset" style={{ marginTop: 20 }}>
+                <H4>Were you able to get tested at this location?</H4>
+                <RadioGroup
+                  aria-label="gender"
+                  name="gender1"
+                  // value={value}
+                  // onChange={handleChange}
+                >
+                  {LOCATION_PROFILE_RADIO_OPTIONS.map((option) => (
+                    <FormControlLabel
+                      key={option.key}
+                      value={option.key}
+                      control={<StyledRadio />}
+                      label={option.displayName}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </Article>
+
+            <Article>
+              <H3>Test Center Offerings</H3>
+              <div>
+                <H4>Select all offerings that exist at this test center:</H4>
+                <Checkboxes>
+                  {OFFERINGS.map((option, index) => (
+                    <Checkbox key={option.key}>
+                      <img src={option.icon} alt={`Select ${option.displayName}`} style={{ margin: 'auto' }} />
+                      <label htmlFor={option.key}>{option.displayName}</label>
+                      <input
+                        type="checkbox"
+                        id={option.key}
+                        checked={option.value}
+                        onChange={this.handleChange.bind(this, option, index)}
+                        hidden
+                      ></input>
+                    </Checkbox>
+                  ))}
+                </Checkboxes>
+              </div>
+              <div>
+                <H4>Select all screening methods used at this test center:</H4>
+                <Checkboxes>
+                  {SCREENING_METHODS.map((option, index) => (
+                    <Checkbox key={option.key} checked={this.state.checked}>
+                      <img src={option.icon} alt={`Select ${option.displayName}`} style={{ margin: 'auto' }} />
+                      <label htmlFor={option.key}>{option.displayName}</label>
+                      <input
+                        type="checkbox"
+                        id={option.key}
+                        checked={option.value}
+                        onChange={this.handleChange.bind(this, option, index)}
+                        hidden
+                      ></input>
+                    </Checkbox>
+                  ))}
+                </Checkboxes>
+              </div>
+            </Article>
+            <ButtonContainer>
+              <CancelButton>Cancel</CancelButton>
+              <SubmitButton color="primary" type="submit">
+                Submit Test Center
+              </SubmitButton>
+            </ButtonContainer>
+          </form>
         </Container>
-      </section>
+      </Section>
     );
   }
 }
 
-const StyledHeader = styled(Header)`
-  .color-block {
-    height: 100vh;
+const Section = styled.section`
+  background: ${(props) => props.theme.palette.gradient.mobile};
+  ${(props) => props.theme.breakpoints.up('md')} {
+    background: ${(props) => props.theme.palette.gradient.desktop};
   }
 `;
 
-const FormHeading = styled.h3`
+const H3 = styled.h3`
   margin-bottom: 20px;
   font-size: 24px;
-  color: #fff;
 `;
 
-const Form = styled.form`
+const H4 = styled.h4`
+  margin-bottom: 20px;
+  font-size: 16px;
+`;
+
+const StyledHeader = styled(Header)`
+  .color-block {
+    display: none;
+  }
+`;
+
+const Article = styled.article`
+  padding: 44px 0;
+  color: #fff;
+
+  &:first-child {
+    border-bottom: 1px solid rgba(255, 255, 255, 0.4);
+  }
+
   > * {
     width: 100%;
-    margin-bottom: 20px;
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
   }
+`;
+
+const Checkboxes = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(172px, 1fr));
+  column-gap: 17px;
+  row-gap: 17px;
+  margin-bottom: 40px;
+`;
+
+const Checkbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 122.78px;
+  text-align: center;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.5);
+`;
+
+const StyledRadio = styled(Radio)`
+  color: #fff;
+  &.Mui-checked {
+    color: #fff;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  column-gap: 32px;
+  row-gap: 32px;
+  margin-bottom: 61px;
+
+  button {
+    font-size: 17px;
+  }
+`;
+
+const CancelButton = styled(PrimaryButton)`
+  color: #fff;
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
+const SubmitButton = styled(PrimaryButton)`
+  color: ${(props) => props.theme.palette.primary.main};
+  background-color: #fff;
 `;
