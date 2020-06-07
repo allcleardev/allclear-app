22630e0474bbf95b533f01ef65efe0ab0c9c5c85
carrie <15806602+carriestreed@users.noreply.github.com>
2020-06-07 19:50:54 -0400

    ALLCLEAR-560 created add-test-center constants

---
+++ src/constants/add-test-center.constants.js
@@ -0,0 +1,108 @@
+import AlertTriangle from '@assets/images/add-testcenter-alert-triangle.svg';
+import Calendar from '@assets/images/add-testcenter-calendar.svg';
+import Car from '@assets/images/add-testcenter-car.svg';
+import DiagnosticTest from '@assets/images/add-testcenter-diagnostic-test.svg';
+import DollarSign from '@assets/images/add-testcenter-dollar-sign.svg';
+import Donate from '@assets/images/add-testcenter-donate.svg';
+import Id from '@assets/images/add-testcenter-id.svg';
+import InsuranceCard from '@assets/images/add-testcenter-insurance-card.svg';
+import Monitor from '@assets/images/add-testcenter-monitor.svg';
+import Swab from '@assets/images/add-testcenter-swab.svg';
+import Transportation from '@assets/images/add-testcenter-transportation.svg';
+import Users from '@assets/images/add-testcenter-users.svg';
+
+export const LOCATION_PROFILE_RADIO_OPTIONS = [
+  {
+    displayName: 'Yes',
+    key: 'yes',
+    value: false,
+  },
+  {
+    displayName: 'No',
+    key: 'no',
+    value: false,
+  },
+  {
+    displayName: 'Did not try',
+    key: 'not_applicable',
+    value: false,
+  },
+];
+
+export const OFFERINGS = [
+  {
+    displayName: 'Antibody Test',
+    icon: DiagnosticTest,
+    key: 'ii',
+    value: false,
+  },
+  {
+    displayName: 'Nasal Swab Test',
+    icon: Swab,
+    key: 'rp',
+    value: false,
+  },
+  {
+    displayName: 'Drivethrough',
+    icon: Car,
+    key: 'driveThru',
+    value: false,
+  },
+  {
+    displayName: 'Plasma Donation',
+    icon: Donate,
+    key: 'canDonatePlasma',
+    value: false,
+  },
+  {
+    displayName: 'Free or Low-Cost Testing',
+    icon: DollarSign,
+    key: 'freeOrLowCost',
+    value: false,
+  },
+  {
+    displayName: 'Online Scheduling',
+    icon: Monitor,
+    key: 'teleScreeningAvailable',
+    value: false,
+  },
+];
+
+export const SCREENING_METHODS = [
+  {
+    displayName: 'Accepts Insurance',
+    icon: InsuranceCard,
+    key: 'acceptsInsurance',
+    value: false,
+  },
+  {
+    displayName: 'Government ID Required',
+    icon: Id,
+    key: 'governmentIdRequired',
+    value: false,
+  },
+  {
+    displayName: 'Referral Required',
+    icon: Users,
+    key: 'referralRequired',
+    value: false,
+  },
+  {
+    displayName: 'Appointment Required',
+    icon: Calendar,
+    key: 'appointmentRequired',
+    value: false,
+  },
+  {
+    displayName: 'First Responders Prioritized',
+    icon: Transportation,
+    key: 'firstResponderFriendly',
+    value: false,
+  },
+  {
+    displayName: 'Minimum Age',
+    icon: AlertTriangle,
+    key: 'minimumAge',
+    value: false,
+  },
+];
