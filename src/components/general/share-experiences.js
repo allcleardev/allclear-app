fc10603d2f0c9f13d5a76dc566d263200fe26ed9
= <=>
2020-06-30 18:58:08 -0400

    AC 484 feature: added tooltip to describe button on hover and changed logic so if user is logged in they will always view rate experience modal

--- src/components/general/share-experiences.js
+++ src/components/general/share-experiences.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'; 
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'; 
+import Tooltip from '@material-ui/core/Tooltip';
 
 import PromptLoginModal from '@general/modals/prompt-login-modal';  
 import WaitExperiencesModal from '@general/modals/wait-experiences-modal'; 
 import ShareExperiencesModal from '@general/modals/share-experiences-modal';
 
-import ModalService from '@services/modal.service'; 
-import ExperienceService from '@services/experience.service';   
+import ModalService from '@services/modal.service';  
 
 import { AppContext } from '@contexts/app.context';
 
@@ -14,33 +14,28 @@ export default function RateExperience() {
     const { appState } = useContext(AppContext);  
     const isLoggedIn = appState.sessionId ? true : false;
 
-    const modalService = ModalService.getInstance(); 
-    const experienceService = ExperienceService.getInstance();  
+    const modalService = ModalService.getInstance();  
 
     const handleRateExperience = async () => {     
         if(!isLoggedIn){  
             modalService.toggleModal('promptLogin', true); 
         } else {   
-            let response = await experienceService.search(appState.sessionId, {});
- 
-            if(response && response.total > 0){ 
-                modalService.toggleModal('promptWaitExperiences', true); 
-            } else { 
-                modalService.toggleModal('promptRateExperiences', true); 
-            }            
+            modalService.toggleModal('promptRateExperiences', true);       
         }    
       };
 
   return (
-    <>
-        <Button
-            variant="contained"
-            color="primary"
-            style={{ borderRadius:15 }}
-            onClick={handleRateExperience.bind(this)}
-        >
-            Share Experience
-        </Button>  
+    <> 
+        <Tooltip title="Got tested here? Share your experience!" placement="left">
+            <Button
+                variant="contained"
+                color="primary"
+                style={{ borderRadius:15 }}
+                onClick={handleRateExperience.bind(this)}
+            >
+                Share Experience
+            </Button>   
+        </Tooltip>
         <PromptLoginModal /> 
         <WaitExperiencesModal /> 
         <ShareExperiencesModal />
